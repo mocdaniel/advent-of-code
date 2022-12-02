df74d33967b85137de0b8d6da5734d66f781b96c
Daniel Bodky <dbodky@gmail.com>
2022-12-02 09:16:22 +0100

    Fix tests for day 02

--- day02/day02_test.go
+++ day02/day02_test.go
@@ -6,13 +6,13 @@ func TestCalculateScore(t *testing.T) {
 	input := `A Y
 B X
 C Z`
-	score := GetTotalScore(input, resultMapA)
+	score := GetTotalScore(input, ResultMapA)
 
 	if score != 15 {
 		t.Errorf("Got: %v; Want: %v", score, 15)
 	}
 
-	score = GetTotalScore(input, resultMapB)
+	score = GetTotalScore(input, ResultMapB)
 
 	if score != 12 {
 		t.Errorf("Got: %v; Want: %v", score, 12)
